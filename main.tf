02986193b17d42fbc6f3ca62ba635d3f57d00189
Nelly Keppens <nelly.aporado@exocoder.io>
2025-09-16 16:52:27 +0200

    Added .gitignore and initial Terraform root setup

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 # Our root configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0" # latest major version
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  #   use_azure_cli = true
+}
+
+
+#Test resource just to see if Terraform and Azure is connected.
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-secure-infra-lab"
+  location = "East US"
+}
