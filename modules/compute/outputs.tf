5b180d11c470a1a3155a21f3b6ec3fdf0bb38fe9
Gent Becode <preetam.beneluxsc@gmail.com>
2025-09-19 11:16:44 +0200

    add outputs for bastion VM public IP and private VM ID

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,9 +1,9 @@
-output "bastion_vm_id" {
-  description = "The ID of the bastion VM"
-  value       = "" # replace with resource reference
+output "bastion_vm_public_ip" {
+  description = "Public IP address of the bastion VM"
+  value       = azurerm_public_ip.bastion_ip.ip_address
 }
 
 output "private_vm_id" {
   description = "The ID of the private VM"
-  value       = "" # replace with resource reference
+  value = azurerm_linux_virtual_machine.private_vm.id
 }
