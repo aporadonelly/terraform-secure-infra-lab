96cf96c445f2c35d7c5726365af19c8206bff953
Nelly Keppens <nelly.aporado@exocoder.io>
2025-09-20 16:35:37 +0200

    Add NSGs and networking module updates

--- main.tf
+++ main.tf
@@ -11,4 +11,11 @@ module "network" {
   source              = "./modules/network"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.resource_group_location
+
+  vnet_cidr           = var.vnet_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+
+  enable_rdp_rule    = var.enable_rdp_rule
+  allowed_admin_cidr = var.allowed_admin_cidr
 }
--- providers.tf
+++ providers.tf
@@ -9,14 +9,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-
-  # Optional: Remote state backend
-  # backend "azurerm" {
-  #   resource_group_name  = "terraform-state-rg"
-  #   storage_account_name = "tfstateaccount"
-  #   container_name       = "tfstate"
-  #   key                  = "secure-infra.tfstate"
-  # }
 }
 
 provider "azurerm" {
