ad2af200e9740badfccf2faccbf3b1cbc1a8fd29
Patrick <patrick.bikorimana.tech@gmail.com>
2025-09-27 19:52:30 +0200

    fixed security/keyVault feature

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,10 +1,21 @@
 # Key Vault Module: stores SSH key and secrets
 
-# Example placeholder resource
-# resource "azurerm_key_vault" "this" {
-#   name                = var.kv_name
-#   location            = var.location
-#   resource_group_name = var.resource_group
-#   tenant_id           = var.tenant_id
-#   sku_name            = "standard"
-# }
+resource "azurerm_key_vault" "this" {
+  name                = var.kv_name
+  location            = var.location
+  resource_group_name = var.resource_group
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+
+  # This block is essential for setting permissions.
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+    ]
+  }
+}
--- modules/keyvault/outputs.tf
+++ modules/keyvault/outputs.tf
@@ -2,3 +2,13 @@ output "key_vault_id" {
   description = "The ID of the Key Vault"
   value       = "" # replace with azurerm_key_vault.this.id
 }
+
+output "id" {
+  value       = azurerm_key_vault.main.id
+  description = "The ID of the created Key Vault"
+}
+
+output "kv_name" {
+  value       = azurerm_key_vault.main.name
+  description = "The name of the created Key Vault"
+}
